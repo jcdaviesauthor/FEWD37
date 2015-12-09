70e59d7ab3bef67c8ef48ca00acec0346246a10a
jcdaviesauthor <thejessicadavies@gmail.com>
2015-12-09 11:17:41 -0800

    Fix all stop lights "On" mode

--- stopLightHw.js
+++ stopLightHw.js
@@ -6,26 +6,42 @@ var buttonGo= document.querySelector(".buttonGo");
 var buttonCaution= document.querySelector(".buttonCaution");
 
 buttonStop.addEventListener ("click", fButtonStop);
-// buttonSlow.addEventListener ("click", fButtonSlow);
-// buttonGo.addEventListener ("click", fButtonGo);
+buttonSlow.addEventListener ("click", fButtonSlow);
+buttonGo.addEventListener ("click", fButtonGo);
 // buttonCaution.addEventListener ("click", fButtonCaution);
 
 function fButtonStop (event) {
 console.log("this works");
 
 // reset func here
+// document.getElementById("myForm").reset();
 
 var stopLightRed= document.querySelector(".circleTopOn");
 stopLightRed.classList.remove("stopLightOff");
 }
 
-//
-// //now use the traffic lights - need 3
-// var redLightOn = document.querySelector()
+function fButtonSlow (event) {
+console.log("this works");
 
+// reset func here
 
-// var dKey = document.querySelector("#d");
-// dKey.addEventListener ("click", playD);
-// function playD (event){
-//   howl.play("D")
-// }
+var stopLightYellow= document.querySelector(".circleMiddleOn");
+stopLightYellow.classList.remove("stopLightOff");
+}
+
+function fButtonGo (event) {
+console.log("this works");
+
+// reset func here
+//caution function
+var stopLightCaution= document.querySelector(".circleMiddleOn");
+stopLightCaution.classList.remove("stopLightOff");
+}
+function fButtonGo (event) {
+console.log("this works");
+
+// reset func here
+
+var stopLightGreen= document.querySelector(".circleBottomOn");
+stopLightGreen.classList.remove("stopLightOff");
+}
