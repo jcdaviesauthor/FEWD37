e6198ae67d2c3c97127c92bd84e8aa250e2538d7
jcdaviesauthor <thejessicadavies@gmail.com>
2015-12-09 18:53:12 -0800

    Trying to blink

--- stopLightHw.js
+++ stopLightHw.js
@@ -8,43 +8,58 @@ var buttonCaution= document.querySelector(".buttonCaution");
 buttonStop.addEventListener ("click", fButtonStop);
 buttonSlow.addEventListener ("click", fButtonSlow);
 buttonGo.addEventListener ("click", fButtonGo);
-// buttonCaution.addEventListener ("click", fButtonCaution);
-
-function fButtonStop (event) {
-console.log("this works");
-
-// reset func here
-// document.getElementById("myForm").reset();
+buttonCaution.addEventListener ("click", fButtonCaution);
 
 var stopLightRed= document.querySelector(".circleTopOn");
+var stopLightYellow= document.querySelector(".circleMiddleOn");
+var stopLightGreen= document.querySelector(".circleBottomOn");
+
+function fButtonStop (event) {
+console.log("this works red");
+resetTurnGrey();
+// reset func here
+// document.getElementById("myForm").reset();
 stopLightRed.classList.remove("stopLightOff");
 }
 
 function fButtonSlow (event) {
-console.log("this works");
-
+console.log("this works yellow");
+resetTurnGrey();
 // reset func here
-
-var stopLightYellow= document.querySelector(".circleMiddleOn");
 stopLightYellow.classList.remove("stopLightOff");
 }
+function resetTurnGrey (){
+  console.log("this reset to grey works");
+  stopLightRed.classList.add("stopLightOff");
+  stopLightYellow.classList.add("stopLightOff");
+  stopLightGreen.classList.add("stopLightOff");
+}
 
-//caution function
-// function fButtonCaution (event) {
-// console.log("this works");
-//
-// // reset func here
+// //caution function
+// setInterval function (event) {
+// console.log("this Caution works");
+// resetTurnGrey();
 // //toggle function here?
-// var stopLightCaution= document.querySelector(".circleMiddleOn");
-
+// setInterval(toggleSlow, 1000);
 // stopLightCaution.classList.remove("stopLightOff");
 // }
+//caution function
+function fButtonCaution (event) {
+console.log("this Caution works");
+resetTurnGrey();
+//toggle function here?
+stopLightYellow.setInterval(toggleSlow, 1000);
+stopLightCaution.classList.remove("stopLightOff");
+}
+
+function toggleSlow (){
+  stopLightYellow.classList.add("stopLightOff");
+
+}
 
 function fButtonGo (event) {
 console.log("this works");
-
+resetTurnGrey();
 // reset func here
-
-var stopLightGreen= document.querySelector(".circleBottomOn");
 stopLightGreen.classList.remove("stopLightOff");
 }
