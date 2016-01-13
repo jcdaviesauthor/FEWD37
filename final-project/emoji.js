b03d4a930d3bee53f0aaf35aba6737b728cccaf9
jcdaviesauthor <thejessicadavies@gmail.com>
2016-01-13 15:49:15 -0800

    Adding array for translation

--- final-project/emoji.js
+++ final-project/emoji.js
@@ -1,35 +1,35 @@
-//add NOUNS table to page, add append verbs to
-//Event listener queery selector = $("")
-var nounEvent = $(".noun");
-$(".noun").on( "click", showNouns);
+  //When we click on anything with class noun show run function showNouns table
+  var nounEvent = $(".noun");
+  $(".noun").on( "click", showNouns);
 
-  // inside this you need a click an event listener for when they click on "piggy"
+  //Changing class of table from display none to displayed
   var clickedNounEl;
   function showNouns (event){
     $(".noun-table").addClass("tableOn");
     clickedNounEl = event.target;
   };
-
+//When click on nouns inside the table run function chooseNouns
   $(".nounImg").on( "click", chooseNouns);
   function chooseNouns(nounEvent){
-        //keeping track of what they clicked on
+      //keeping track of what they clicked on
       var imgClicked = nounEvent.target
       console.log (imgClicked);
 
-      // need to update to jquery
+      //clone/recreate the image in the spot on the page because we want it to remain in the table as well
+      // NEED TO UPDATE TO JQUERY
       var clonedImg= document.createElement("img")
+      //setting attribute to image
       clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
-
+      //the noun we clicked has no text content
       clickedNounEl.textContent = "";
+      //then we are appending the new cloned image to the page
       clickedNounEl.appendChild(clonedImg);
   };
 
-  //add VERB table to page, add append verbs to
-  //Event listener query selector = $("")
+  //VERB
   var verbEvent = $(".verb");
   $(".verb").on( "click", showVerbs);
 
-    // inside this you need a click an event listener for when they click on "piggy"
     var clickedVerbEl;
     function showVerbs (event){
       $(".verb-table").addClass("tableOn");
@@ -38,24 +38,21 @@ $(".noun").on( "click", showNouns);
 
     $(".verbImg").on( "click", chooseVerb);
     function chooseVerb(verbEvent){
-          //keeping track of what they clicked on
-        var imgClicked = verbEvent.target
-        console.log (imgClicked);
+        var imgVerbClicked = verbEvent.target
+        console.log (imgVerbClicked);
 
-        // need to update to jquery
-        var clonedImg= document.createElement("img")
-        clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
+        // NEED TO UPDATE TO JQUERY
+        var clonedVerbImg= document.createElement("img")
+        clonedVerbImg.setAttribute("src", imgVerbClicked.getAttribute("src"))
 
         clickedVerbEl.textContent = "";
-        clickedVerbEl.appendChild(clonedImg);
+        clickedVerbEl.appendChild(clonedVerbImg);
     };
 
-    //add ADJECTIVES table to page, add append verbs to
-    //Event listener query selector = $("")
+    //add ADJECTIVES
     var adjectiveEvent = $(".adjective");
     $(".adjective").on( "click", showAdjective);
 
-      // inside this you need a click an event listener for when they click on "piggy"
       var clickedAdjectiveEl;
       function showAdjective (event){
         $(".adjective-table").addClass("tableOn");
@@ -64,20 +61,18 @@ $(".noun").on( "click", showNouns);
 
       $(".adjectiveImg").on( "click", chooseAdjective);
       function chooseAdjective(adjectiveEvent){
-            //keeping track of what they clicked on
-          var imgClicked = adjectiveEvent.target
-          console.log (imgClicked);
+          var imgAdjectivesClicked = adjectiveEvent.target
+          console.log (imgAdjectivesClicked);
 
-          // need to update to jquery
-          var clonedImg= document.createElement("img")
-          clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
+          // NEED TO UPDATE TO JQUERY
+          var clonedAdjectiveImg= document.createElement("img")
+          clonedAdjectiveImg.setAttribute("src", imgAdjectivesClicked.getAttribute("src"))
 
           clickedAdjectiveEl.textContent = "";
-          clickedAdjectiveEl.appendChild(clonedImg);
+          clickedAdjectiveEl.appendChild(clonedAdjectiveImg);
       };
 
 //Create array ("buckets") for nouns
-//how to link photos in an array?
 var nounArray =
   [
     {
@@ -106,6 +101,22 @@ var nounArray =
     },
 
   ];
-// debugger
 
-//Do same for verbs and adjectives
+//Do same array for verbs and adjectives
+
+//FOR LOOP to put translation on the page
+//When click on translate button run function to replace ALL emojis
+// var translateBtn = $("button");
+// //or is it replace emojis????
+// $("button").on( "click", replaceNouns);
+//
+// function replaceNouns() {
+//   //should this be path??? or class noun?
+//   var $span= $("path");
+//
+//   for (var i = 0; i < nounArray.length;i++) {
+//         // var nounTranslation = document.createElement("span")
+//         span.textContent = nounArray[i].translation;
+//         // append translation to span with
+//         span.appendChild(translation);
+//   }
