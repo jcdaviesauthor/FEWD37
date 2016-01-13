142257d20f0f09ec34881e9b1f0c489e31440c85
jcdaviesauthor <thejessicadavies@gmail.com>
2016-01-12 22:54:34 -0800

    All of categories to append image to page

--- final-project/emoji.js
+++ final-project/emoji.js
@@ -1,4 +1,4 @@
-
+//add NOUNS table to page, add append verbs to
 //Event listener queery selector = $("")
 var nounEvent = $(".noun");
 $(".noun").on( "click", showNouns);
@@ -16,12 +16,65 @@ $(".noun").on( "click", showNouns);
       var imgClicked = nounEvent.target
       console.log (imgClicked);
 
+      // need to update to jquery
       var clonedImg= document.createElement("img")
       clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
 
       clickedNounEl.textContent = "";
       clickedNounEl.appendChild(clonedImg);
   };
+
+  //add VERB table to page, add append verbs to
+  //Event listener query selector = $("")
+  var verbEvent = $(".verb");
+  $(".verb").on( "click", showVerbs);
+
+    // inside this you need a click an event listener for when they click on "piggy"
+    var clickedVerbEl;
+    function showVerbs (event){
+      $(".verb-table").addClass("tableOn");
+      clickedVerbEl = event.target;
+    };
+
+    $(".verbImg").on( "click", chooseVerb);
+    function chooseVerb(verbEvent){
+          //keeping track of what they clicked on
+        var imgClicked = verbEvent.target
+        console.log (imgClicked);
+
+        // need to update to jquery
+        var clonedImg= document.createElement("img")
+        clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
+
+        clickedVerbEl.textContent = "";
+        clickedVerbEl.appendChild(clonedImg);
+    };
+
+    //add ADJECTIVES table to page, add append verbs to
+    //Event listener query selector = $("")
+    var adjectiveEvent = $(".adjective");
+    $(".adjective").on( "click", showAdjective);
+
+      // inside this you need a click an event listener for when they click on "piggy"
+      var clickedAdjectiveEl;
+      function showAdjective (event){
+        $(".adjective-table").addClass("tableOn");
+        clickedAdjectiveEl = event.target;
+      };
+
+      $(".adjectiveImg").on( "click", chooseAdjective);
+      function chooseAdjective(adjectiveEvent){
+            //keeping track of what they clicked on
+          var imgClicked = adjectiveEvent.target
+          console.log (imgClicked);
+
+          // need to update to jquery
+          var clonedImg= document.createElement("img")
+          clonedImg.setAttribute("src", imgClicked.getAttribute("src"))
+
+          clickedAdjectiveEl.textContent = "";
+          clickedAdjectiveEl.appendChild(clonedImg);
+      };
 
 //Create array ("buckets") for nouns
 //how to link photos in an array?
@@ -38,10 +91,21 @@ var nounArray =
     },
 
     {
+      "path": "images/noun-emoji/noun-monkey.png",
+      "translation": "Monkey"
+    },
+
+    {
       "path": "images/noun-emoji/noun-piggy.png",
       "translation": "Piggy"
-    }
+    },
+
+    {
+      "path": "images/noun-emoji/noun-shit.png",
+      "translation": "Shit"
+    },
+
   ];
-debugger
+// debugger
 
 //Do same for verbs and adjectives
