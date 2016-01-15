6b3cf22575df746f8aeda5087f748ceacd6ca270
jcdaviesauthor <thejessicadavies@gmail.com>
2016-01-15 12:52:33 -0800

    fix table appear disappear spend world instead of emoji

--- final-project/emoji.js
+++ final-project/emoji.js
@@ -6,32 +6,48 @@
   var clickedNounEl;
   function showNouns (event){
     $(".noun-table").addClass("tableOn");
+      //keeping track of where in the story you clicked on
     clickedNounEl = event.target;
-    // console.log (clickedNounEl);
+    console.log (clickedNounEl);
 
   };
 //When click on nouns inside the table run function chooseNouns
+//eventlistener
   $(".nounImg").on( "click", chooseNouns);
-  function chooseNouns(nounEvent){
-      //keeping track of what they clicked on
-      var imgClicked = nounEvent.target;
-      console.log (imgClicked);
 
+  function chooseNouns(nounEvent){
+      //keeping track of what emoji they clicked on
+      var imgClicked = nounEvent.target;
+      // var $(this) = imgClicked;
+
+      console.log("the emoji you clicked is", imgClicked);
+      var tableClicked = $(imgClicked).parents('table');
       //clone/recreate the image in the spot on the page because we want it to remain in the table as well
       // NEED TO UPDATE TO JQUERY
       var clonedImg= document.createElement("img");
       //setting attribute to image
       clonedImg.setAttribute("src", imgClicked.getAttribute("src"));
+      clonedImg.setAttribute("data-translation", imgClicked.getAttribute("data-translation"))
+
+      //RESIZING EMOJI
+      clonedImg.setAttribute("class", "emoji storyEmoji");
+      // clonedImg.setAttribute("class", "storyEmoji");
+
       // Need these to get the data translation
-      clonedImg.setAttribute("data-translation", imgClicked.getAttribute("data-translation"));
-      clonedImg.setAttribute('class', 'madlib-emoji');
-      //the noun we clicked has no text content
+      // clonedImg.setAttribute("data-translation", imgClicked.getAttribute("data-translation"));
+      // clonedImg.setAttribute('class', 'madlib-emoji');
+      //the noun we clicked has no text content, takes awy the word noun
       clickedNounEl.textContent = "";
       // NEW ADD- DOES NOT WORK??????
       // $(".noun").addClass("wordOffEmogiOn");
       //then we are appending the new cloned image to the page
       clickedNounEl.appendChild(clonedImg);
+      clearTable(tableClicked);
   };
+
+  function clearTable(displayedTable){
+      displayedTable.removeClass("tableOn");
+  }
 
   //VERB
   var verbEvent = $(".verb");
@@ -41,12 +57,14 @@
     function showVerbs (event){
       $(".verb-table").addClass("tableOn");
       clickedVerbEl = event.target;
+
     };
 
     $(".verbImg").on( "click", chooseVerb);
     function chooseVerb(verbEvent){
         var imgVerbClicked = verbEvent.target
         console.log (imgVerbClicked);
+        var tableClicked = $(imgVerbClicked).parents('table');
 
         // NEED TO UPDATE TO JQUERY
         var clonedVerbImg= document.createElement("img")
@@ -54,6 +72,7 @@
 
         clickedVerbEl.textContent = "";
         clickedVerbEl.appendChild(clonedVerbImg);
+        clearTable(tableClicked);
     };
 
     //add ADJECTIVES
@@ -64,19 +83,23 @@
       function showAdjective (event){
         $(".adjective-table").addClass("tableOn");
         clickedAdjectiveEl = event.target;
+
       };
 
       $(".adjectiveImg").on( "click", chooseAdjective);
       function chooseAdjective(adjectiveEvent){
           var imgAdjectivesClicked = adjectiveEvent.target
           console.log (imgAdjectivesClicked);
-
+          var tableClicked = $(imgAdjectivesClicked).parents('table');
+          console.log (tableClicked);
           // NEED TO UPDATE TO JQUERY
           var clonedAdjectiveImg= document.createElement("img")
           clonedAdjectiveImg.setAttribute("src", imgAdjectivesClicked.getAttribute("src"))
+          clonedAdjectiveImg.setAttribute("data-translation", imgAdjectivesClicked.getAttribute("data-translation"))
 
           clickedAdjectiveEl.textContent = "";
           clickedAdjectiveEl.appendChild(clonedAdjectiveImg);
+          clearTable(tableClicked);
       };
 
 //Create array ("buckets") for nouns
@@ -169,26 +192,35 @@ var nounArray =
 
 // FOR LOOP to put translation on the page
 // When click on translate button run function to replace ALL emojis
-var translateBtn = $("button");
-//or is it replace emojis????
+// var translateBtn = $("button");
+// //or is it replace emojis????
 $("button").on( "click", replaceEmojis);
 
 function replaceEmojis() {
   //UPDATE TO JQUERY
   //get parent
-  var images = document.querySelectorAll('.madlib-emoji');
+  var images = document.querySelectorAll(".storyEmoji");
 
-  // for (var i = 0; i < images.length; i++) {
-  //   images[i]
-//then for loop
-  for (var i = 0; i < nounArray.length; i++) {
-    [i]
-    // remove child
-    var remove child = element.removeChild(".madlib-emoji");
-    //get translation from data
-    // set prents text content
-    span.textContent = nounArray[i].data-translation;
-    //append to page
-    span.appendChild(textContent);
-  }
+  for (var i = 0; i < images.length; i++) {
+  var eachEmojiName = images[i].getAttribute("data-translation");
+  console.log(eachEmojiName);
+  // add class emoji off
+  // images[i].style.display.none;
+  var span = images[i].parentNode;
+  span.textContent = eachEmojiName;
+  // $("body").append(textPage);
 }
+}
+
+  // var tableClicked = $(imgClicked).parents('table');
+//then for loop
+//
+//     // remove child
+//     var remove child = element.removeChild(".madlib-emoji");
+//     //get translation from data
+//     // set prents text content
+//     span.textContent = nounArray[i].data-translation;
+//     //append to page
+//     span.appendChild(textContent);
+//   }
+// }
